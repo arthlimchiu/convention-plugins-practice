940acd6239e062ce9b540979aa9a770b54b0aa37
Arth Limchiu <limchiuarth@gmail.com>
2024-02-27 09:15:17 +0800

    Add build-logic project as composite build

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,16 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
